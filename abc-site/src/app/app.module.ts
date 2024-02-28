8a77f67bb77cdaa63b56cd5ab175b07104ea39f4
aaknox <azhya.knox@gmail.com>
2024-02-27 20:09:00 -0600

    added navbar + edited about and contact pages

--- abc-site/src/app/app.module.ts
+++ abc-site/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
